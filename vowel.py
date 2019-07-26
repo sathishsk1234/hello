27924f765f694ff79b2fd04adb30726c82d28075
Sathishkumar.R <51990643+sathishsk1234@users.noreply.github.com>
2019-07-26 15:27:25 +0530

    Delete vowel.py

--- vowel.py
+++
@@ -1,8 +0,0 @@
-a=str(input())
-b='a','e','i','o','u','A','E','I','O','U'
-if a in ('!','@','#','$',"%",'^','&','*'):
-    print("invalid")
-elif a in b:
-    print("vowel")
-else:
-    print('constant')
